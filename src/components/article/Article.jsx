42f7c1769d88c10782c8a661cf6d866ef5d8eb36
sannydasilva <sannydasilvasimoes@gmail.com>
2023-12-06 15:04:40 +0100

    responsive composant article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -24,112 +24,114 @@ const Article = () => {
     <>
       <Title />
       <Menu />
-      <img className="img-etageres3" src="./etageres3.jpg" alt="" />
-      <div className="modif-etagere">
-        <h2>Etagères sur mesure</h2>
-        250.00 €<h3>Essence de Bois</h3>
-        <div className="ui form">
-          <div className="inline fields">
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input type="radio" name="frequency" />
-                <label>Frêne</label>
+      <div className="article-container">
+        <img className="img-etageres3" src="./etageres3.jpg" alt="" />
+        <div className="modif-etagere">
+          <h2>Etagères sur mesure</h2>
+          250.00 €<h3>Essence de Bois</h3>
+          <div className="ui form">
+            <div className="inline fields">
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="frequency" />
+                  <label>Frêne</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="frequency" />
+                  <label>Chêne</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="frequency" />
+                  <label>Noyer</label>
+                </div>
+              </div>
+              <div className="field">
+                <div className="ui radio checkbox">
+                  <input type="radio" name="frequency" />
+                  <label>Chêne</label>
+                </div>
               </div>
             </div>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input type="radio" name="frequency" />
-                <label>Chêne</label>
+          </div>
+          <h3>Dimensions</h3>
+          <div className="ui progress">
+            <div className="bar">
+              <div
+                className="progress"
+                style={{
+                  width: progressWidth,
+                }}
+              ></div>
+            </div>
+            <div className="label">Longueur: {length} cm</div>
+          </div>
+          <div className="ignored">
+            <div className="ui icon buttons">
+              <div
+                className="decrement ui basic inverted red button icon"
+                onClick={handleDecrement}
+              >
+                <i className="minus icon"></i>
+              </div>
+              <div
+                className="increment ui basic inverted green button icon"
+                onClick={handleIncrement}
+              >
+                <i className="plus icon"></i>
               </div>
             </div>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input type="radio" name="frequency" />
-                <label>Noyer</label>
+          </div>
+          <div className="ui progress">
+            <div className="bar">
+              <div
+                className="progress"
+                style={{
+                  width: progressWidth,
+                }}
+              ></div>
+            </div>
+            <div className="label">Largeur: {length} cm</div>
+          </div>
+          <div className="ignored">
+            <div className="ui icon buttons">
+              <div
+                className="decrement ui basic inverted red button icon"
+                onClick={handleDecrement}
+              >
+                <i className="minus icon"></i>
+              </div>
+              <div
+                className="increment ui basic inverted green button icon"
+                onClick={handleIncrement}
+              >
+                <i className="plus icon"></i>
               </div>
             </div>
-            <div className="field">
-              <div className="ui radio checkbox">
-                <input type="radio" name="frequency" />
-                <label>Chêne</label>
-              </div>
-            </div>
           </div>
-        </div>
-        <h3>Dimensions</h3>
-        <div className="ui progress">
-          <div className="bar">
-            <div
-              className="progress"
-              style={{
-                width: progressWidth,
-              }}
-            ></div>
-          </div>
-          <div className="label">Longueur: {length} cm</div>
-        </div>
-        <div className="ignored">
-          <div className="ui icon buttons">
-            <div
-              className="decrement ui basic inverted red button icon"
-              onClick={handleDecrement}
-            >
-              <i className="minus icon"></i>
-            </div>
-            <div
-              className="increment ui basic inverted green button icon"
-              onClick={handleIncrement}
-            >
-              <i className="plus icon"></i>
-            </div>
-          </div>
-        </div>
-        <div className="ui progress">
-          <div className="bar">
-            <div
-              className="progress"
-              style={{
-                width: progressWidth,
-              }}
-            ></div>
-          </div>
-          <div className="label">Largeur: {length} cm</div>
-        </div>
-        <div className="ignored">
-          <div className="ui icon buttons">
-            <div
-              className="decrement ui basic inverted red button icon"
-              onClick={handleDecrement}
-            >
-              <i className="minus icon"></i>
-            </div>
-            <div
-              className="increment ui basic inverted green button icon"
-              onClick={handleIncrement}
-            >
-              <i className="plus icon"></i>
-            </div>
-          </div>
-        </div>
-        <div className="epaisseur">
-          <div className="label">Epaisseur</div>
+          <div className="epaisseur">
+            <div className="label">Epaisseur</div>
 
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input type="checkbox" name="frequency" />
-              <label>3</label>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input type="checkbox" name="frequency" />
+                <label>3</label>
+              </div>
+            </div>
+            <div className="field">
+              <div className="ui radio checkbox">
+                <input type="checkbox" name="frequency" />
+                <label>4</label>
+              </div>
             </div>
           </div>
-          <div className="field">
-            <div className="ui radio checkbox">
-              <input type="checkbox" name="frequency" />
-              <label>4</label>
-            </div>
-          </div>
+          <button className="button-panier">Ajouter au panier</button>
+          <br />
+          <button className="button-commande">Commander et payer</button>
         </div>
-        <button className="button-panier">Ajouter au panier</button>
-        <br />
-        <button className="button-commande">Commander et payer</button>
       </div>
     </>
   );
